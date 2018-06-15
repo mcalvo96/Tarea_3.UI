c076049af4a3d494e51a909177d4cf35db79af16
mcalvo96 <31193305+mcalvo96@users.noreply.github.com>
2018-06-15 01:04:13 -0600

    cambios en el proyecto

---
+++ Tarea_3.Dominio.UTL/Respuesta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3.Dominio.UTL
+{
+    public partial class Respuesta<T>
+    {
+        public Boolean bnlIndicadorTransaccion = true;
+        public EMessageType enuTipoMensaje;
+        public String strMensajeRespuesta = "¡Transacción realizada exitosamente!";
+        public String strOrigen = String.Empty;
+        public int resultado;
+        public T valorRetorno;
+    }//Fin de la clase Response.
+
+    #region "Enums"
+    public enum EMessageType
+    {
+        eNone = 0,
+        eValidation = 1,
+        eInformative = 2,
+        eError = 3,
+        eRequest = 4
+    }//Fin del EMessageType.
+    #endregion
+}
